b40c172337e8dbfa0edb9724ecccd4a8b3dcbfe2
arijguest <117730068+arijguest@users.noreply.github.com>
2024-11-13 23:58:07 +0000

    Update 3D-MI.py

--- 3D-MI.py
+++ 3D-MI.py
@@ -212,7 +212,3 @@ def index():
         HTML_TEMPLATE,
         cesium_token=CESIUM_ION_ACCESS_TOKEN
     )
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
